06ff10c47f44157704fa88cbd13f39945f90481e
Tetsuaki Baba <info@tetsuakibaba.jp>
2018-07-02 00:11:42 +0900

    add: clean.sh for publish

--- clean.sh
+++ clean.sh
@@ -5,4 +5,7 @@ rm  ./Examples/annotation/bin/data/yolov2-tiny.weights
 rm  ./Examples/single_image/bin/data/yolov2-tiny.cfg
 rm  ./Examples/camera/bin/data/yolov2-tiny.cfg
 rm  ./Examples/annotation/bin/data/yolov2-tiny.cfg
+rm -fR ./Examples/camera/bin/cameraDebug.app
+rm -fR ./Examples/single_image/bin/single_imageDebug.app
+rm -fR ./Examples/annotation/bin/annotationDebug.app
 echo "done"
