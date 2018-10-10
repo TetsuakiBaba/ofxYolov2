5c87f7e5c7556f282bbccbb780c34575c3362963
kkshmz <kyeshimizu@gmail.com>
2018-10-10 17:00:14 +0900

    rearrange scripts and example project files

--- clean.sh
+++ clean.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-rm  ./Examples/camera/bin/data/yolov2-tiny.weights 
-rm  ./Examples/single_image/bin/data/yolov2-tiny.weights 
-rm  ./Examples/annotation/bin/data/yolov2-tiny.weights 
-rm  ./Examples/single_image/bin/data/yolov2-tiny.cfg
-rm  ./Examples/camera/bin/data/yolov2-tiny.cfg
-rm  ./Examples/annotation/bin/data/yolov2-tiny.cfg
-rm -fR ./Examples/camera/bin/camera*.app
-rm -fR ./Examples/single_image/bin/single_image*.app
-rm -fR ./Examples/annotation/bin/annotation*.app
+rm  ./example_camera/bin/data/yolov2-tiny.weights 
+rm  ./example_single_image/bin/data/yolov2-tiny.weights 
+rm  ./example_annotation/bin/data/yolov2-tiny.weights 
+rm  ./example_single_image/bin/data/yolov2-tiny.cfg
+rm  ./example_camera/bin/data/yolov2-tiny.cfg
+rm  ./example_annotation/bin/data/yolov2-tiny.cfg
+rm -fR ./example_camera/bin/camera*.app
+rm -fR ./example_single_image/bin/single_image*.app
+rm -fR ./example_annotation/bin/annotation*.app
 echo "done"
--- getYolov2Weights.sh
+++ getYolov2Weights.sh
@@ -3,13 +3,13 @@ get:
 path_to_cfg="https://raw.githubusercontent.com/pjreddie/darknet/master/cfg/yolov2-tiny.cfg"
 path_to_weights="https://pjreddie.com/media/files/yolov2-tiny.weights"
 wget ${path_to_weights}
-cp ./yolov2-tiny.weights ./Examples/camera/bin/data/yolov2-tiny.weights 
-cp ./yolov2-tiny.weights ./Examples/single_image/bin/data
-cp ./yolov2-tiny.weights ./Examples/annotation/bin/data
+cp ./yolov2-tiny.weights ./example_camera/bin/data/yolov2-tiny.weights 
+cp ./yolov2-tiny.weights ./example_single_image/bin/data
+cp ./yolov2-tiny.weights ./example_annotation/bin/data
 wget ${path_to_cfg} 
-cp ./yolov2-tiny.cfg ./Examples/single_image/bin/data/
-cp ./yolov2-tiny.cfg ./Examples/camera/bin/data/
-cp ./yolov2-tiny.cfg ./Examples/annotation/bin/data/
+cp ./yolov2-tiny.cfg ./example_single_image/bin/data/
+cp ./yolov2-tiny.cfg ./example_camera/bin/data/
+cp ./yolov2-tiny.cfg ./examples_annotation/bin/data/
 rm -f ./yolov2-tiny.cfg
 rm -f ./yolov2-tiny.weights
 echo "done"
